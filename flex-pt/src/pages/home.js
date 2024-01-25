b13e56e784439fff5e506149ddb27d0974b2ba35
JongHoPark <qkrwhdgh126@gmail.com>
2024-01-25 17:57:25 +0900

    feat: homepage

---
+++ flex-pt/src/pages/home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../styles/home.scss";
+
+export default function HomePage() {
+  return (
+    <div className="home">
+      <div className="blur"></div>
+    </div>
+  );
+}
