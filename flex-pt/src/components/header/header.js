5f8fff672ad47e362d83d056178e68c79f065544
JongHoPark <qkrwhdgh126@gmail.com>
2024-01-29 11:04:18 +0900

    feat: 반응형 header

--- flex-pt/src/components/header/header.js
+++ flex-pt/src/components/header/header.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import "./header.scss";
 import Logo from "../../images/logo/flex-pt-logo.jpeg";
+import { IoMdMenu } from "react-icons/io";
 
 export default function Header() {
+  const hd = useRef();
+  const hd_rd = useRef();
+
+  // 넓이 950 도달시 변환
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      if (window.innerWidth < 950) {
+        hd.current.style.display = "none";
+        hd_rd.current.style.display = "flex";
+      } else if (window.innerWidth > 950) {
+        hd.current.style.display = "flex";
+        hd_rd.current.style.display = "none";
+      }
+    });
+  }, []);
   return (
     <header>
-      <div className="inline">
+      <div className="inline" ref={hd}>
         <div className="left">
           <a className="logo" href="/">
             <img src={Logo} alt="" />
@@ -36,6 +52,16 @@ export default function Header() {
             </li>
           </ul>
         </div>
+      </div>
+      <div className="inline-reduct" ref={hd_rd}>
+        <div className="menu">
+          <IoMdMenu />
+        </div>
+        <a className="logo" href="/">
+          <img src={Logo} alt="" />
+          <div className="logotext">FLEX PT</div>
+        </a>
+        <div></div>
       </div>
     </header>
   );
