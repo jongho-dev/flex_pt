690dd493e1431bf72ca3ce21c8d229d9d7abde1b
JongHoPark <qkrwhdgh126@gmail.com>
2024-02-15 18:55:54 +0900

    feat: loginpage POST

--- flex-pt/src/pages/login/login.js
+++ flex-pt/src/pages/login/login.js
@@ -1,13 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "./login.scss";
 
 export default function LoginPage() {
+  const [id, setId] = useState("");
+  const [pw, setPw] = useState("");
+
+  const handleInputId = (e) => {
+    setId(e.target.value);
+  };
+
+  const handleInputPw = (e) => {
+    setPw(e.target.value);
+  };
+
+  const onSubmit = (e) => {
+    fetch("http://localhost:5000/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ id: id, pw: pw }),
+    });
+  };
+
   return (
     <div className="login">
-      <form action="">
+      <form action="" onSubmit={onSubmit}>
         <div className="loginlogo">LOG IN</div>
-        <input className="id" placeholder="아이디" />
-        <input type="password" className="pw" placeholder="비밀번호" />
+        <input
+          className="id"
+          placeholder="아이디"
+          onChange={handleInputId}
+          value={id}
+        />
+        <input
+          type="password"
+          className="pw"
+          placeholder="비밀번호"
+          onChange={handleInputPw}
+          value={pw}
+        />
         <button>로그인</button>
         <div className="links">
           <a href="">회원가입</a>
